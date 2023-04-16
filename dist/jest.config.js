80af005fe529e1b757c3d34745b44dbc0ade35ae
u032582 <u032582@gmail.com>
2023-04-16 13:49:55 +0900

    init

---
+++ dist/jest.config.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const config = {
+    testEnvironment: 'node',
+    roots: ['<rootDir>/test'],
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest'
+    }
+};
+exports.default = config;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiamVzdC5jb25maWcuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvamVzdC5jb25maWcudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQSxNQUFNLE1BQU0sR0FBVztJQUNyQixlQUFlLEVBQUUsTUFBTTtJQUN2QixLQUFLLEVBQUUsQ0FBQyxnQkFBZ0IsQ0FBQztJQUN6QixTQUFTLEVBQUUsQ0FBQyxjQUFjLENBQUM7SUFDM0IsU0FBUyxFQUFFO1FBQ1QsYUFBYSxFQUFFLFNBQVM7S0FDekI7Q0FDRixDQUFDO0FBQ0Ysa0JBQWUsTUFBTSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHR5cGUgeyBDb25maWcgfSBmcm9tICdqZXN0JztcblxuY29uc3QgY29uZmlnOiBDb25maWcgPSB7XG4gIHRlc3RFbnZpcm9ubWVudDogJ25vZGUnLFxuICByb290czogWyc8cm9vdERpcj4vdGVzdCddLFxuICB0ZXN0TWF0Y2g6IFsnKiovKi50ZXN0LnRzJ10sXG4gIHRyYW5zZm9ybToge1xuICAgICdeLitcXFxcLnRzeD8kJzogJ3RzLWplc3QnXG4gIH1cbn07XG5leHBvcnQgZGVmYXVsdCBjb25maWc7XG4iXX0=
